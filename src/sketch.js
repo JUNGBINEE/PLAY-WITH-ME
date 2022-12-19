878269955226646934c69b248b62ef4a0cabdc7f
myeongjunkim <ilikerunning@naver.com>
2022-12-19 23:05:06 +0900

    feat: piano MVP

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,53 @@
+const WHITE_FREQ={
+    'C':262,
+    'D':294,
+    'E':330,
+    'F':349,
+    'G':392,
+    'A':440,
+    'B':494,
+    'C-high':523
+}
+
+const BLACK_FREQ={
+    'C#':277,
+    'D#':311,
+    'F#':370,
+    'G#':415,
+    'A#':466,
+}
+
+let whiteKeyBoards = [];
+let blackKeyBoards = [];
+let osc = new p5.Oscillator('sine');
+
+function createWhiteKeyBoard() {
+    let notes = Object.keys(WHITE_FREQ);
+    for (let i = 0; i < 8; i++) {
+        let white = new WhiteKeyBoard(0, i* windowHeight/8, notes.shift());
+        whiteKeyBoards.push(white);
+    }
+}
+
+function createBlackKeyBoard() {
+    let notes = Object.keys(BLACK_FREQ);
+    for (let i = 0; i < 7; i++) {
+        if( i!= 2 && i!= 6 ) {
+            let black = new BlackKeyBoard(
+                    windowWidth/6, 
+                    windowHeight/16 + windowHeight/80  + i* windowHeight/8,
+                    notes.shift()
+                );
+            blackKeyBoards.push(black);
+        }
+    }
+}
+
 class WhiteKeyBoard {
-    constructor(x, y) {
+    constructor(x, y, note) {
         this.x = x;
         this.y = y;
+        this.note = note
     }
     
     show() {
@@ -10,14 +56,24 @@ class WhiteKeyBoard {
     }
 
     clicked(){
-        console.log('white');
+        if (mouseX < windowWidth/6 && // 1/3 of the piano
+            mouseX > this.x && 
+            mouseX < this.x + windowWidth/2 && 
+            mouseY > this.y && 
+            mouseY < this.y + windowHeight/8
+        ) {
+            osc.amp(20)
+            osc.freq(WHITE_FREQ[this.note], 0.5);
+            console.log(this.note, WHITE_FREQ[this.note]);
+        }
     }
 }
 
 class BlackKeyBoard {
-    constructor(x, y) {
+    constructor(x, y, note) {
         this.x = x;
         this.y = y;
+        this.note=note
     }
     
     show() {
@@ -26,14 +82,45 @@ class BlackKeyBoard {
     }
 
     clicked(){
-        console.log('black');
+       if ( mouseX > windowWidth/6 && // 1/3 of the piano
+            mouseX > this.x &&
+            mouseX < this.x + windowWidth/3 && 
+            mouseY > this.y && 
+            mouseY < this.y + windowHeight/10
+        ) {
+            osc.amp(20)
+            osc.freq(BLACK_FREQ[this.note], 1);
+            console.log(this.note, BLACK_FREQ[this.note]);
+        }
     }
+
 }
 
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-}
+    let cnv = createCanvas(windowWidth, windowHeight);
+    
+    
+    getAudioContext().resume();
+
+    createWhiteKeyBoard();
+    createBlackKeyBoard();
+
+    
+    cnv.mouseClicked(function() {
+        osc.start();
+        blackKeyBoards.forEach(black => {
+            black.clicked();
+        });
+        whiteKeyBoards.forEach(white => {
+            white.clicked();
+        });
+        osc.amp(0, 0.5);
+    });
+
+};
+
+
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
@@ -45,14 +132,12 @@ function draw() {
 
 
 function drawPiano() {
-    for (let i = 0; i < 8; i++) {
-        let white = new WhiteKeyBoard(0, i* windowHeight/8);
-        white.show();
-    }
-    for (let i = 0; i < 7; i++) {
-        if( i!= 2 && i!= 6 ) {
-            let black = new BlackKeyBoard(windowWidth/6, windowHeight/16 + windowHeight/80  + i* windowHeight/8);
-            black.show();
-        }
-    }
+    whiteKeyBoards.forEach(whiteKeyBoard => {
+        whiteKeyBoard.show();
+    });
+    blackKeyBoards.forEach(blackKeyBoard => {
+        blackKeyBoard.show();
+    });
 }
+
+
